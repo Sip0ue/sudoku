59bf206590f4fa6c99c05853e23336236a2af3cf
benjamin guilhot <guilhot.benjamin@conduent.com>
2026-09-24 11:16:41 +0200

    final init

--- sudoku/src/main/java/com/mycompany/sudoku/Sudoku.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.sudoku;
-
-public class Sudoku {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
---
+++ sudoku/src/main/java/com/mycompany/sudoku/SudokuSolver.java
@@ -0,0 +1,34 @@
+package com.mycompany.sudoku;
+
+/**
+* SudokuSolver.
+*
+* TODO 4: FIll in your names and student IDs:
+*
+* @author Levi von Doornik
+* @id 2416131
+* @author Benjamin Guilhot
+* @id 2576740
+*/
+
+public class SudokuSolver {
+    
+    public SudokuSolver(SudokuGrid grid) {
+        // Initialize the SudokuSolver with the provided SudokuGrid
+    }
+    
+    public boolean solve() {
+        // Use a recursive strategy to solve the Sudoku puzzle
+        return false;
+    }
+    
+    public void solveIt() {
+        // Use solve() to solve the puzzle and print the solution
+        //or a message if no solution is found
+    }
+    
+    public static void main(String[] args) {
+        // Create a SudokuGrid and a SudokuSolver
+    }
+    
+}
